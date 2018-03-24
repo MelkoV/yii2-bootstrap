5a8e55ffd6cc52e06dbadb9dc33b8fa63c7b5776
Anton Smorodin <anton@berrysoft.ru>
2018-03-24 12:16:16 +0500

    init

---
+++ common/config/main.php
@@ -0,0 +1,61 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    "language" => "ru-RU",
+    'timeZone' => 'Europe/Moscow',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'class' => 'yii\web\User',
+            'enableAutoLogin' => true,
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['guest', 'user'],
+        ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages'
+                ],
+            ],
+        ],
+        /*'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\DbMessageSource',
+                    'db' => 'db',
+                    'sourceLanguage' => 'en-US',
+                    'sourceMessageTable' => 'language_source',
+                    'messageTable' => 'language_translate',
+                    'cachingDuration' => 86400,
+                    'enableCaching' => true,
+                ],
+            ],
+        ],*/
+		'log' => [
+            'targets' => [
+                [
+//                    'traceLevel' => YII_DEBUG ? 3 : 0,
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+                [
+                    'class' => 'yii\log\EmailTarget',
+                    'levels' => ['error'],
+//                    'categories' => ['yii\db\*'],
+                    "except" => ['yii\web*'],
+                    'message' => [
+                        'from' => ['robot@example.com'],
+                        'to' => ['me@example.com'],
+                        'subject' => 'ERROR on example.com',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
